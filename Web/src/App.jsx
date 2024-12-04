3d4109ff8913228afb8733449571b3facc701806
Blaž Perko <perkoblaz3@gmail.com>
2024-12-04 13:21:24 +0100

    Added forgot password dialog
    Added forgot password dialog
    
    Co-Authored-By: Igor Radošević <144734664+IgorRado1@users.noreply.github.com>

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Typography } from '@mui/material';
-import LoginPage from './LoginPage'; // Uvoz LoginPage.jsx
-import RegisterPage from './RegisterPage'; // Uvoz RegisterPage.jsx
-import HelloUser from './HelloUser'; // Uvoz HelloUser.jsx
+import LoginPage from './components/LoginPage'; // Uvoz LoginPage.jsx
+import RegisterPage from './components/RegisterPage'; // Uvoz RegisterPage.jsx
+import HelloUser from './components/HelloUser'; // Uvoz HelloUser.jsx
 
 const App = () => {
     return (
         <Router>
             <div className="App">
-                <br/>
-                {/*<Typography variant="h1" align="center"> </Typography>*/}
+            <br />
+               {/*<Typography variant="h1" align="center"> </Typography>*/}
                 <Routes>
-                    <Route path="/" element={<LoginPage/>}/>
-                    <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/register" element={<RegisterPage/>}/>
-                    <Route path="/Hello" element={<HelloUser/>}/>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/Hello" element={<HelloUser />} />
                 </Routes>
             </div>
         </Router>
