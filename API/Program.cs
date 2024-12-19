6fe6df18945c288c2cc1e10e832cfe431a803ff2
Bart <bartolomej.kozorog@gmail.com>
2024-12-19 11:01:48 +0700

    remove unused package-lock & dir, throw error on missing config

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
-var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>() ?? throw new Exception("Missing JWT configuration");
 var key = Encoding.ASCII.GetBytes(jwtSettings.Secret);
 
 builder.Services.AddAuthentication(options =>
