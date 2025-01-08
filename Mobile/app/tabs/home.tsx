8b3e9c427e55e2bcf3343f81221bf0eb1c70e8dc
ModricFX <modric.agar@gmail.com>
2025-01-08 14:18:51 +0100

    turned IOS and ANDROID into 1 file

--- Mobile/app/tabs/home.tsx
+++ Mobile/app/tabs/home.tsx
@@ -1,7 +1,8 @@
 ﻿import { Platform } from 'react-native';
 import HomeAndroid from './homeAndroid';
 import HomeIOS from './homeIOS';
+import realHome from './realHome';
 
 const HomeScreen = Platform.OS === 'ios' ? HomeIOS : HomeAndroid;
 
-export default HomeScreen;
+export default realHome;
