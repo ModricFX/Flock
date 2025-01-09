db17fc7a7aa88711d815a377e04caa8ad2636c48
tomazcede <tomi.cede1@gmail.com>
2025-01-09 10:27:43 +0100

    Fix

--- API/Controllers/EventController.cs
+++ API/Controllers/EventController.cs
@@ -9,7 +9,7 @@ namespace flock.Controllers
 {
     [Route("api/event")]
     [ApiController]
-    
+    [Authorize]
     public class EventController : ControllerBase
     {
         private readonly IEventRepository _eventRepository;
